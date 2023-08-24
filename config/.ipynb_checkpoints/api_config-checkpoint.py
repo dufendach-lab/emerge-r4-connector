21ad4960849152eecede2fdd7be6c606c01609f1
ekc1998 <ekc1998@gmail.com>
2023-08-23 22:01:39 -0400

    adding clinical variables sync script

---
+++ config/.ipynb_checkpoints/api_config-checkpoint.py
@@ -0,0 +1,10 @@
+
+config = dict(
+    R4_api_token=b'B290AA73E00C9A8F2401F108258BBCF9',
+    R4_api_url=b'https://redcap.vanderbilt.edu/api/',
+    R4copy_api_token=b'B07F0A9D9FE03F1A0FCF31AEEAE1DAC4',
+    R4copy_api_url=b'https://redcap.research.cchmc.org/api/',
+    R4_testing_api_token=b'DF9D58CC45922B8E576E96E7A0B7D54F',
+    interface_user='eMerge',
+    interface_pass='eMerge'
+    )
